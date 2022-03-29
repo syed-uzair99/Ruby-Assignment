eb31861dc1406a811c5ec6ba01f719dd031c8b0f
syed-uzair99 <102572779+syed-uzair99@users.noreply.github.com>
2022-03-29 11:06:35 +0530

    Update 1_ASCII.rb

--- 1_ASCII.rb
+++ 1_ASCII.rb
@@ -1,4 +1,6 @@
-
+=begin
+Assignment 1: AsciI Art creation, you can choose any ASCII art and display the output as art. This is to test your looping and control structure knowledge (1 hour)
+=end
 
 class DE
     def trees(d)
