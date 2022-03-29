f72ae405820f8ae4bd7e9cc4a4c2ae7eb13ffe8f
syed-uzair99 <102572779+syed-uzair99@users.noreply.github.com>
2022-03-29 11:00:51 +0530

    Create 1_ASCII.rb

---
+++ 1_ASCII.rb
@@ -0,0 +1,45 @@
+
+
+class DE
+    def trees(d)
+        for i in 0..d
+            k=d-i-1
+            for j in 0..k
+                print " "
+            end
+            for j in k..(d-1)
+                if j==k
+                    print "/"
+                elsif j==(d-1)
+                    print "\\"
+                else
+                    print "**"
+                end
+            end
+            puts "\n"
+        end
+        for i in 0..d*2
+            print "-"
+        end
+    end
+    # trees(10)
+
+    def stem(d)
+        s=d
+        puts "\n"
+        for i in 0..d/2
+            k=s-i-1
+            for i in 0..d*2/3
+                print " "
+            end
+            puts "|||||\n"    
+        end
+    end
+
+end  
+obj=DE.new
+puts "Enter value"
+x=gets.strip
+x=x.to_i
+obj.trees(x)
+obj.stem(x)
